6402fdb0825dcc139dd1c6db92dfe4797305ca4b
이태민 <itaemin@itaemins-MacBook-Pro.local>
2024-06-17 18:38:35 +0900

    Done with the basic UIs of Rock Paper and Scissors game
    Got stuck as I was confused how to use DOM manipulation
    Turns out as I just added some elements in html, things got much simpler.
    Selected the button by grabbing their ID, and change the text content
    also by grabbing their div ID.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,60 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissors");
+const resultDiv = document.querySelector("#result");
+const scoreDiv = document.querySelector("#score");
+
+
 function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
 
-function getHumanChoice(){
-    let sign = prompt("Your turn!");
 
-    return sign;
-}
-
-function playRound(humanChoice,computerChoice) {
-    let human = humanChoice.toUpperCase();
-    let computer = computerChoice.toUpperCase();
+function playRound(humanChoice) {
+    const human = humanChoice.toUpperCase();
+    const computer = getComputerChoice().toUpperCase();
+    let result = '';
 
     const winningComb = { ROCK: "SCISSORS", PAPER: "ROCK", SCISSORS: "PAPER"};
 
     if (human === computer) {
-        alert("It's a draw!");
+        result = "It's a draw!";
     } else if (winningComb[human] === computer) {
-        alert("You won!");
+        humanScore++;
+        result = "You won!";
     } else {
-        alert ("You lost!");
+        computerScore++;
+        result = "You lost!";
     }
+    
+    resultDiv.textContent = result;
+    scoreDiv.textContent = `Score: You - ${humanScore}, Computer - ${computerScore}`;
 
+    if (humanScore === 5 || computerScore === 5){
+        if (humanScore > computerScore){
+            resultDiv.textContent = "You won the game! Reload to play the game again.";
+        } else {
+            resultDiv.textContent = "You Lost. Reload to try again!";
+        }
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorButton.disabled = true;
+    }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+rockButton.addEventListener("click", () =>{
+    playRound("Rock");
+});
+
+paperButton.addEventListener("click", () =>{
+    playRound("Paper");
+});
+
+scissorButton.addEventListener("click", () =>{
+    playRound("Scissors");
+});
